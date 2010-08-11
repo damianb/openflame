84e2bcb99af8db2903a270c4c9e265fd4942138e
Damian Bushong <141002651+damianb@users.noreply.github.com>
2010-08-10 21:03:31 -0500

    ohgod I lied I'm so sorry D:

--- OpenFlame/OfJSON.php
+++ OpenFlame/OfJSON.php
@@ -5,7 +5,7 @@
  * @copyright (c) 2010 OpenFlameCMS.com
  * @license http://opensource.org/licenses/mit-license.php The MIT License
  *
- * Minimum Requirement: PHP 5.0.0
+ * Minimum Requirement: PHP 5.2.0
  */
 
 if(!defined('ROOT_PATH'))
