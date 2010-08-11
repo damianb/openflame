a4e34784f5cac71d895367ac964584bba31fe73c
Obsidian <stratosphere.programming@gmail.com>
2010-08-10 20:42:03 -0500

    commit the new OfJSON class (for easy JSON integration) along with the start of some framework exceptions.

---
+++ OpenFlame/OfException.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *
+ * @package OpenFlame Web Framework
+ * @copyright (c) 2010 OpenFlameCMS.com
+ * @license http://opensource.org/licenses/mit-license.php The MIT License
+ *
+ * Minimum Requirement: PHP 5.0.0
+ */
+
+if(!defined('ROOT_PATH'))
+	define('ROOT_PATH', './');
+
+/**
+ * OpenFlame Web Framework - Primary Exception class,
+ * 		Extension of the default Exception class.
+ *
+ *
+ * @author      Damian Bushong ("Obsidian")
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ */
+class OfException extends Exception
+{
+	// wat
+	const ERR_WTF = 0;
+}
+
+/**
+ * OpenFlame Web Framework - JSON Exception class,
+ * 		Used for exceptions generated in the JSON library.
+ *
+ *
+ * @author      Damian Bushong ("Obsidian")
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ */
+class JSONException extends OfException
+{
+	const ERR_JSON_NO_FILE = 4000;
+	const ERR_JSON_UNKNOWN = 4100;
+	const ERR_JSON_NO_ERROR = 4101;
+	const ERR_JSON_DEPTH = 4102;
+	const ERR_JSON_CTRL_CHAR = 4103;
+	const ERR_JSON_SYNTAX = 4104;
+}
---
+++ OpenFlame/OfJSON.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ *
+ * @package OpenFlame Web Framework
+ * @copyright (c) 2010 OpenFlameCMS.com
+ * @license http://opensource.org/licenses/mit-license.php The MIT License
+ *
+ * Minimum Requirement: PHP 5.0.0
+ */
+
+if(!defined('ROOT_PATH'))
+	define('ROOT_PATH', './');
+
+/**
+ * OpenFlame Web Framework - JSON handling class,
+ * 		OOP interface for use with JSON files/strings.
+ *
+ *
+ * @author      Damian Bushong ("Obsidian")
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @note        This class should not be instantiated; it should only be statically accessed.
+ */
+class OfJSON
+{
+	/**
+	 * Builds a JSON string based on input.
+	 * @param mixed $data - The data to cache.
+	 * @return string - JSON string.
+	 */
+	public static function encode($data)
+	{
+		return json_encode($data);
+	}
+
+	/**
+	 * Loads a JSON string or file and returns the data held within.
+	 * @param string $json - The JSON string or the path of the JSON file to decode.
+	 * @param boolean $is_file - Are we loading from a JSON file?
+	 * @return array - The contents of the JSON string/file.
+	 * @throws JSONException
+	 */
+	public static function decode($json, $is_file = true)
+	{
+		if($is_file)
+		{
+			if(!file_exists($json))
+				throw new JSONException('JSON file does not exist', JSONException::ERR_JSON_NO_FILE);
+			$json = file_get_contents($json);
+		}
+
+		$data = json_decode(preg_replace('#\#.*?' . PHP_EOL . '#', '', $json), true);
+
+		if($data === NULL)
+		{
+			switch(json_last_error())
+			{
+				case JSON_ERROR_NONE:
+					$error = 'No error';
+					$code = JSONException::ERR_JSON_NO_ERROR;
+				break;
+
+				case JSON_ERROR_DEPTH:
+					$error = 'Maximum JSON recursion limit reached.';
+					$code = JSONException::ERR_JSON_DEPTH;
+				break;
+
+				case JSON_ERROR_CTRL_CHAR:
+					$error = 'Control character error';
+					$code = JSONException::ERR_JSON_CTRL_CHAR;
+				break;
+
+				case JSON_ERROR_SYNTAX:
+					$error = 'JSON syntax error';
+					$code = JSONException::ERR_JSON_SYNTAX;
+				break;
+
+				default:
+					$error = 'Unknown JSON error';
+					$code = JSONException::ERR_JSON_UNKNOWN;
+				break;
+			}
+
+			throw new JSONException($error, $code);
+		}
+
+		return $data;
+	}
+}
