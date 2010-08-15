60ac5ff56a6e8135f2b15d0462ec401b64a9796e
David King <imkingdavid@gmail.com>
2010-08-15 15:20:48 -0400

    allows file uploads, moves them to the specified directory. I made a change in OfInput.php because in _cleanVar(),  and  are being called in the else statement, but are not defined. I replaced them with , which seems to work, but if they breaks it we can figure out a fix. Also, I added _FILES to the globals array because it wasn't there and _REQUEST wasn't doing what I needed

--- OpenFlame/OfFile.php
+++ OpenFlame/OfFile.php
@@ -12,7 +12,7 @@ if(!defined('ROOT_PATH'))
 	define('ROOT_PATH', './');
 
 // Make sure we have our class here
-if(!class_exsits('OfInput') && file_exists(ROOT_PATH . 'OfInput.php'))
+if(!class_exists('OfInput') && file_exists(ROOT_PATH . 'OfInput.php'))
 	require ROOT_PATH . 'OfInput.php';
 else if(!file_exists(ROOT_PATH . 'OfInput.php') && !class_exists('OfInput'))
 	exit;
@@ -22,9 +22,26 @@ else if(!file_exists(ROOT_PATH . 'OfInput.php') && !class_exists('OfInput'))
  * 	     Extends the Input class to handle file uploads.
  *
  *
- * @author      Sam Thompson ("Sam")
+ * @author      David King ("imkingdavid")
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  */
 class OfFile extends OfInput
 {
+	/**
+	* Constructor 
+	*
+	* @param string $file The file form field name e.g. <input type="file" name="the_file" /> would make $file = 'the_file';
+	* @param string $destionation_dir The directory for the file to be uploaded to
+	*/
+	function __construct($file, $destinationDir)
+	{
+		parent::__construct($file, array('' => ''), '_FILES');
+		
+		move_uploaded_file($this->rawInput['tmp_name'], $destinationDir . $this->rawInput['name']);
+	}
+	
+	/**
+	* @TODO: Alright, so currently this takes a form-uploaded file and moves it to a destination directory.
+	*		I still need to verify file type and size and such.
+	*/	
 }
