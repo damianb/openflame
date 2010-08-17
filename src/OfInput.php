b39c3bd9c4ab1e79d97469e6e0c34738b87f8789
David King <imkingdavid@gmail.com>
2010-08-16 22:42:24 -0400

    changed camelCase variables to lowercase_with_underscore as per the coding guidelines.

--- src/OfInput.php
+++ src/OfInput.php
@@ -24,37 +24,37 @@ class OfInput
 	/**
 	 * @var mixed raw input
 	 */
-	protected $rawInput;
+	protected $raw_input;
 
 	/**
 	 * @var mixed cleaned input
 	 */
-	protected $cleanedInput;
+	protected $cleaned_input;
 
 	/**
 	 * Constructor
 	 *
-	 * @param string $varName Var name in the global you're after
+	 * @param string $var_name Var name in the global you're after
 	 * @param mixed $default Default value and type to fall back on and check for good types
-	 * @param string @globalName The name of the super global to use. REQUEST, GET, POST, COOKIE, and SERVER are all avavible.
+	 * @param string @global_name The name of the super global to use. REQUEST, GET, POST, COOKIE, and SERVER are all avavible.
 	 */
-	public function __construct($varName, $default, $globalName = '_REQUEST')
+	public function __construct($var_name, $default, $global_name = '_REQUEST')
 	{
 		// Prepend the _ if not there
-		if($globalName[0] != '_')
-			$globalName = '_' . $globalName;
+		if($global_name[0] != '_')
+			$global_name = '_' . $global_name;
 
 		// We should have a good global now.
-		$globalName = in_array($globalName, array('_REQUEST', '_GET', '_POST', '_COOKIE', '_SERVER', '_FILES')) ? $globalName : '_REQUEST';
+		$global_name = in_array($global_name, array('_REQUEST', '_GET', '_POST', '_COOKIE', '_SERVER', '_FILES')) ? $global_name : '_REQUEST';
 
 		// We need to make sure that cookie is not contaminating the value of request
-		if($globalName == '_REQUEST' && isset($_COOKIE[$varName]))
-			$_REQUEST[$varName] = isset($_POST[$varName]) ? $_POST[$varName] : $_GET[$varName];
+		if($global_name == '_REQUEST' && isset($_COOKIE[$var_name]))
+			$_REQUEST[$var_name] = isset($_POST[$var_name]) ? $_POST[$var_name] : $_GET[$var_name];
 
 		// Assign the raw var
-		$this->rawInput = $GLOBALS[$globalName][$varName];
+		$this->raw_input = $GLOBALS[$global_name][$var_name];
 
-		$this->cleanedInput = $this->cleanVar($GLOBALS[$globalName][$varName], $default);
+		$this->cleaned_input = $this->cleanVar($GLOBALS[$global_name][$var_name], $default);
 	}
 
 	/**
@@ -68,10 +68,10 @@ class OfInput
 	{
 		if(is_array($var))
 		{
-			list($_keyDefault, $_valueDefault) = each($default);
+			list($_key_default, $_value_default) = each($default);
 
 			foreach($var as $key => $value)
-				$var[$this->bindVar($key, $_keyDefault)] = $this->cleanVar($value, $_valueDefault);
+				$var[$this->bindVar($key, $_key_default)] = $this->cleanVar($value, $_value_default);
 		}
 		else
 		{
@@ -118,7 +118,7 @@ class OfInput
 			case 'email':
 				// By "James Watts and Francisco Jose Martin Moreno"
 				// Assumed Public Domain
-				return preg_match("#^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$#i", $this->cleanedInput) === 1 ? true : false;
+				return preg_match("#^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$#i", $this->cleaned_input) === 1 ? true : false;
 			break;
 
 			case 'url':
@@ -126,19 +126,19 @@ class OfInput
 				// Assumed Public Domain
 				// @TODO Test cases to make sure it works.
 				// 		One issue was pointed out in the comments that should be looked into
-				return preg_match("#((https?://)?([-\w]+\.[-\w\.]+)+\w(:\d+)?(/([-\w/_\.]*(\?\S+)?)?)*)#", $this->cleanedInput) === 1 ? true : false;
+				return preg_match("#((https?://)?([-\w]+\.[-\w\.]+)+\w(:\d+)?(/([-\w/_\.]*(\?\S+)?)?)*)#", $this->cleaned_input) === 1 ? true : false;
 			break;
 
 			case 'ip4':
 				// By "G. Andrew Duthie" (http://regexlib.com/REDetails.aspx?regexp_id=32)
 				// Assumed Public Domain
-				return preg_match("#^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$#", $this->cleanedInput) === 1 ? true : false;
+				return preg_match("#^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$#", $this->cleaned_input) === 1 ? true : false;
 			break;
 
 			case 'ip6':
 				// By "Stephen Ryan" (http://forums.dartware.com/viewtopic.php?t=452)
 				// Assumed Public Domain
-				return preg_match("#\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$#i", $this->cleanedInput) === 1 ? true : false;
+				return preg_match("#\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$#i", $this->cleaned_input) === 1 ? true : false;
 			break;
 
 			// Alpha-numeric chars only
@@ -146,17 +146,17 @@ class OfInput
 				// Check if they wanted a range
 				$range = ($max || $min && $max > $min) ? '{' . $min . ',' . $max . '}' : '';
 
-				return preg_match("#^[A-Za-z0-9]*{$range}$#i", $this->cleanedInput) === 1 ? true : false;
+				return preg_match("#^[A-Za-z0-9]*{$range}$#i", $this->cleaned_input) === 1 ? true : false;
 			break;
 
 			// Validate a basic string, uses $min, $max
 			case 'string':
-				return (strlen($this->cleanedInput) >= $min && strlen($this->cleanedInput) <= $max) ? true : false;
+				return (strlen($this->cleaned_input) >= $min && strlen($this->cleaned_input) <= $max) ? true : false;
 			break;
 
 			// Validates any int, uses $min and $max for the value of the int, not the size.
 			case 'int':
-				return ($this->cleanedInput >= $min && $this->cleanedInput <= $max) ? true : false;
+				return ($this->cleaned_input >= $min && $this->cleaned_input <= $max) ? true : false;
 			break;
 		}
 
@@ -171,7 +171,7 @@ class OfInput
 	 */
 	public function getRaw()
 	{
-		return $this->rawInput;
+		return $this->raw_input;
 	}
 
 	/**
@@ -181,7 +181,7 @@ class OfInput
 	 */
 	public function getClean()
 	{
-		return $this->cleanedInput;
+		return $this->cleaned_input;
 	}
 
 	/**
@@ -191,6 +191,6 @@ class OfInput
 	 */
 	public function wasSet()
 	{
-		return $this->rawInput == null ? true : false;
+		return $this->raw_input == null ? true : false;
 	}
 }
