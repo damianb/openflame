fa88c777f2ae627ed3b1bd02f0bcf995d37a5a48
Damian Bushong <141002651+damianb@users.noreply.github.com>
2010-08-18 10:06:56 -0500

    commit some docs and a little more code for the Of class

--- src/Of.php
+++ src/Of.php
@@ -20,22 +20,43 @@ if(!defined('ROOT_PATH'))
  * @author      Damian Bushong ("Obsidian")
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        http://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * @note        This class should not be instantiated; it should only be statically accessed.
  */
 class Of
 {
-	// @todo phpdocs
-	protected $config = array();
-	protected $objects = array();
+	/**
+	 * @var array - Array of settings we have loaded and stored
+	 */
+	protected static $config = array();
 
-	public static function loadConfigFile($filename)
+	/**
+	 * @var array - Array of objects we have instantiated and stored
+	 */
+	protected static $objects = array();
+
+	/**
+	 * Loads an array of settings into the config array.
+	 * @param array $config_ary - The array of settings to load
+	 * @return void
+	 */
+	public static function loadConfig(array $config_ary)
 	{
-		// load the config filename
-		// @todo use json file?  use php file and include?
+		// note: this method can easily be used to load from a JSON config file as well!
+		// Of::loadConfig(OfJSON::decode('./path_to_json/config.json'));
+		self::$config = array_merge(self::$config, $config_ary);
 	}
 
+	/**
+	 * Grab a setting from the config array
+	 * @param string $config_name - The name of the setting to retrieve
+	 * @return mixed - The setting's value if the setting exists, or NULL if the setting does not exist
+	 */
 	public static function config($config_name)
 	{
-		// pull the config we want
+		if(!isset(self::$config[$config_name]))
+			return NULL;
+		return self::$config[$config_name];
 	}
 
 	public static function loader($class)
@@ -53,7 +74,10 @@ class Of
 		// get one of the OpenFlame Framework objects...
 	}
 
-	// alias of Of::getObject()
+	/**
+	 * Alias of self::getObject() for quick coding purposes
+	 * @see Of::getObject()
+	 */
 	public static function obj($slot)
 	{
 		self::getObject($slot);
