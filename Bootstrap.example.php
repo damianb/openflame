dd56bcd6ca0fa86f9bad964485f9b417cd8dac5c
Obsidian <stratosphere.programming@gmail.com>
2010-08-19 15:10:18 -0500

    rename the bootstrap file and include some code to show how to strap up the framework

--- Bootstrap.example.php
+++ Bootstrap.example.php
@@ -6,5 +6,14 @@
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        http://github.com/OpenFlame/OpenFlame-Framework
  *
- * Minimum Requirement: PHP 5.0.0
+ * Minimum Requirement: PHP 5.2.3
  */
+
+define('OF_ROOT', dirname(__FILE__) . '/src/');
+require OF_ROOT . 'src/Of.php';
+
+// Register the autoloader
+spl_autoload_register('Of::loader');
+
+// blah blah blah
+// add to this later
