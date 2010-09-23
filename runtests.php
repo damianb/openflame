d11ec6fd27f316dbe42d6f1e5c0d1d6536747615
Damian Bushong <141002651+damianb@users.noreply.github.com>
2010-09-23 08:47:21 -0500

    a few improvements to the test runner

--- runtests.php
+++ runtests.php
@@ -12,8 +12,17 @@
 define('OF_ROOT', dirname(__FILE__) . '/src/');
 define('OF_TEST_ROOT', dirname(__FILE__) . '/tests/');
 define('IN_OF_TEST', true);
-
 //define('OFCLI_DISABLE_COLORS', true);
+
+/**
+ * @note Edit this to have a test ignored
+ */
+$ignore_tests = array(
+	'OfTestCache.php',
+	'OfTestLanguage.php',
+	'OfTestOf.php',
+	'OfTestTwig.php',
+);
 
 require OF_TEST_ROOT . 'Bootstrap.php';
 
@@ -25,7 +34,7 @@ $tests = array();
 $dir_contents = @scandir(OF_TEST_ROOT . 'tests/');
 foreach($dir_contents as $file)
 {
-	if($file[0] == '.' || substr(strrchr($file, '.'), 1) != 'php' || !is_file(OF_TEST_ROOT . 'tests/' . $file))
+	if($file[0] == '.' || substr(strrchr($file, '.'), 1) != 'php' || !is_file(OF_TEST_ROOT . 'tests/' . $file) || in_array($file, $ignore_tests))
 		continue;
 	$tests[] = $file;
 }
@@ -41,7 +50,11 @@ foreach($tests as $test)
 {
 	if(($include = @include(OF_TEST_ROOT . 'tests/' . $test)) === false)
 		throw new Exception(sprintf('Unable to include test file "%1$s"', $test));
+
 	$test_class = substr($test, 0, strrpos($test, '.'));
+
+	if(!class_exists($test_class, false))
+		throw new Exception(sprintf('Test class "%1$s" missing'));
 
 	/* @var OfTestBase */
 	$obj = new $test_class();
