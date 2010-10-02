10c47947f39ff25a4ac865a30af47c3bb0cd39e9
Sam Thompson <sam@websyntax.net>
2010-10-02 16:18:28 -0700

    No doctrine here

--- src/OfDb.php
+++ src/OfDb.php
@@ -21,5 +21,64 @@ if(!defined('OF_ROOT')) exit;
  */
 class OfDb
 {
+	public $manager;
+	
+	public $connection;
+	
+	private $modelsPath = '';
+	
+	const CONNECTION_NAME = 'openflameframework';
 
+	/**
+	 * Constructor
+	 *
+	 * @param string modelsPath - Path to the models directory
+	 */
+	public function __construct($modelsPath)
+	{
+		// Get Doctrine ready to deploy
+		require OF_ROOT . 'src/vendor/doctrine/Doctrine.php';
+		spl_autoload_register(array('Doctrine', 'autoload'));
+		$this->manager = Doctrine_Manager::getInstance();
+		
+		$this->modelsPath = $modelsPath;
+	}
+
+	/**
+	 * Connects to the database via Doctrine, sets default configuration, and loads models
+	 *
+	 * @param string dsn - connection string
+	 * @param string connecitonName - name of the connection. Leave default to assume default connection or specify custom
+	 *
+	 * @return void
+	 */
+	public function loadDatabase($dsn, $connectionName = '')
+	{
+		$this->connection = Doctrine_Manager::connection($dsn, (($connectionName) ? $connectionName : self::CONNECTION_NAME));
+		
+		$this->connection->setCharset('utf8');
+		$this->connection->setCollate('utf8_bin');
+
+		// Configure some things
+		$this->manager->setAttribute(Doctrine_Core::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+		$this->manager->setAttribute(Doctrine_Core::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+
+		spl_autoload_register(array('Doctrine_Core', 'modelsAutoload'));
+		$this->manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING, Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
+		
+		Doctrine_Core::loadModels($this->modelsPath);
+	}
+
+	/**
+	 * Loads tables
+	 *
+	 * @param array tables - array of all the tables to load
+	 *
+	 * @return void
+	 */
+	public function loadTables($tables = array())
+	{
+		foreach($tables as $table)
+			$this->{"$table"} = Doctrine_Core::getTable($table);
+	}
 }
