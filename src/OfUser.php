8622275427105105c9280ebeb03cfabe5e83f4a9
Sam Thompson <sam@websyntax.net>
2010-10-09 14:13:42 -0700

    Got some code (mostly comments) for OfUser
    This reminds me I have to do some SQL stuff before we learn how to utilize doctrine migrations.

--- src/OfUser.php
+++ src/OfUser.php
@@ -21,4 +21,64 @@ if(!defined('OF_ROOT')) exit;
  */
 class OfUser
 {
+	/**
+	 * @var $data
+	 *
+	 * Contains all the data that is stored in the Users table
+	 * Copy of $_SESSION, and can be used dynamically as such.
+	 */
+	public $data = array();
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+	
+	}
+
+	/**
+	 * Session Start
+	 * Starts a new session
+	 *
+	 * @return void
+	 */
+	public function sessionStart()
+	{
+	}
+
+	/**
+	 * Session Kill
+	 * Destorys a session
+	 *
+	 * @return void
+	 */
+	public function sessionKill()
+	{
+	}
+
+	/**
+	 * Format date
+	 * Takes the user preference for the date format and turns the passed timestap 
+	 * into a valid date.
+	 *
+	 * @param int $ts Timestamp, empty one will result in current time()
+	 */
+	public function formatDate($ts = time())
+	{
+	}
+
+	/**
+	 * Login
+	 * Logs in a user, call when you are receiving a login. Must be called after OfUser::sessionStart()
+	 *
+	 * @param string $username Username of the person to login
+	 * @param string $password Plaintext password as inputed by the user
+	 * @param bool $auto_login Set to true to allow the user to autologin every time after logging in this time
+	 * @param string $redirect_success Path to the page to redirect to after successful login. Defaults to current page
+	 * @param string $redirect_failure Path to the page to redirect to after failed login. Defaults to current page
+	 */
+	public function logIn($username, $password, $auto_login = false, $redirect_success = '', $redirect_failure = '')
+	{
+	}
 }
