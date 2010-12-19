f4cd97ee22e3f8c171bb67a0036e0d0db8f42f02
Damian Bushong <141002651+damianb@users.noreply.github.com>
2010-12-19 09:22:35 -0600

    Port over the cache system, along with the JSON cache engine. Still need to do the serialize() engine however.

--- src/Cache/Engine/EngineInterface.php
+++ src/Cache/Engine/EngineInterface.php
@@ -9,19 +9,24 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework;
+namespace OpenFlame\Framework\Cache\Engine;
 
 if(!defined('OpenFlame\\Framework\\ROOT_PATH')) exit;
 
 /**
- * OpenFlame Web Framework - Some class
- * 	     Some class description.
+ * OpenFlame Web Framework - Cache Engine interface,
+ * 		Cache engine prototype, declares required methods that a cache engine must define in order to be valid.
  *
  *
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class SomeClass
+interface EngineInterface
 {
-	// asdf
+	public function getEngineName();
+	public function build($data);
+	public function load($file);
+	public function exists($file);
+	public function destroy($file);
+	public function store($file, $data);
 }
--- src/Cache/Engine/EngineJSON.php
+++ src/Cache/Engine/EngineJSON.php
@@ -9,19 +9,80 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework;
+namespace OpenFlame\Framework\Cache\Engine;
+use OpenFlame\Framework\Utility\JSON;
 
 if(!defined('OpenFlame\\Framework\\ROOT_PATH')) exit;
 
 /**
- * OpenFlame Web Framework - Some class
- * 	     Some class description.
+ * OpenFlame Web Framework - JSON Cache engine,
+ * 		JSON cache engine for use with the cache interface.
  *
  *
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class SomeClass
+class EngineJSON extends EngineFileBase implements EngineInterface
 {
-	// asdf
+	/**
+	 * @const - The algorithm to use for checksum of the cache file's contents (minus the first two lines)
+	 */
+	const CHECKSUM_ALGO = 'md5';
+
+	/**
+	 * Builds a JSON-based cache file, complete with idiot warning.
+	 * @param mixed $data - The data to cache.
+	 * @return string - Full JSON code to be stored in a cache file.
+	 */
+	public function build($data)
+	{
+		$data = JSON::encode($data);
+
+		return implode("\n", array(
+			'# OpenFlame Web Framework cache file - modify at your own risk!',
+			'# data md5 checksum: ' . hash(self::CHECKSUM_ALGO, $data),
+			$data,
+		));
+	}
+
+	/**
+	 * Loads a JSON cache file and returns the cached data.
+	 * @param string $file - The file to load from.
+	 * @return mixed - The cached data.
+	 */
+	public function load($file)
+	{
+		return JSON::decode($this->readFile("$file.json"));
+	}
+
+	/**
+	 * Check to see if a cache file exists.
+	 * @param string $file - The file to check.
+	 * @return boolean - Has the data been cached?
+	 */
+	public function exists($file)
+	{
+		return $this->fileExists("$file.json");
+	}
+
+	/**
+	 * Destroys a cache file.
+	 * @param string $file - The cache file to destroy.
+	 * @return void
+	 */
+	public function destroy($file)
+	{
+		$this->deleteFile("$file.json");
+	}
+
+	/**
+	 * Stores data to a cache file.
+	 * @param string $file - The cache file to store our data in.
+	 * @param string $data - The data to cache.
+	 * @return void
+	 */
+	public function store($file, $data)
+	{
+		$this->writeFile("$file.json", $data);
+	}
 }
