c527e99c13c87e8a2c1b8859b77433c47036297c
Obsidian <stratosphere.programming@gmail.com>
2010-12-26 15:18:26 -0600

    start work on the static URL router

--- src/URL/Router.php
+++ src/URL/Router.php
@@ -9,19 +9,75 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework;
+namespace OpenFlame\Framework\URL;
 
 if(!defined('OpenFlame\\Framework\\ROOT_PATH')) exit;
 
 /**
- * OpenFlame Web Framework - Some class
- * 	     Some class description.
+ * OpenFlame Web Framework - Static URL router,
+ * 	     A simple and easy to understand static URL router, provides an alternative to the fluid URL handler.
  *
  *
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class SomeClass
+class Router
 {
-	// asdf
+	/**
+	 * @const - Just to make sure no one sends stupidly long url requests for this to process
+	 * @note - Changes to this setting also affect behavior in OpenFlame\Framework\URL\RouteInstance
+	 */
+	const EXPLODE_LIMIT = 15;
+
+	protected $routes = array();
+
+	public function __construct()
+	{
+		// asdf
+	}
+
+	public function newRoutes(array $routes)
+	{
+		foreach($routes as $route)
+			$this->newRoute($route);
+
+		return $this;
+	}
+
+	public function newRoute($route)
+	{
+		list($route_base, ) = explode('/', $route, 2);
+		$this->routes[$route_base][] = new \OpenFlame\Framework\URL\RouteInstance($route);
+		return $this;
+	}
+
+	public function processRequest($request_url)
+	{
+		$request = explode('/', $request_url, self::EXPLODE_LIMIT);
+
+		$request_base = $request[0];
+		// We're cheating a bit here to boost performance under load.
+		if(!isset($this->routes[$request_base]))
+		{
+			// throw 404 error here
+		}
+
+		// We need to verify the request against the routes one by one, and go for the last one that works.
+		// We do this so that routes set later on take priority, so routes can be easily overridden in an application
+		foreach(array_reverse($this->routes[$request_base]) as $route)
+		{
+			if($route->verify($request))
+			{
+				$found = true;
+				break;
+			}
+		}
+
+		if($found !== true)
+		{
+			// throw 404 error here
+		}
+
+		return $route->loadRequest($request);
+	}
 }
