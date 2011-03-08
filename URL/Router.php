fbda975603f57ba01a8eaf9ad4b9fda00d35b0a7
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-03-08 14:16:07 -0600

    Add in a ton of functionality to the router...
    - "Home" route, or the route to fire if we're on the default page
    - "Error" route, or the route to fire if we hit an invalid (unsupported) route
    - Base URL stripping
    - Smarter trailing/leading slash management
    - Better handling of route caching, and added one single method to load the entire previous route cache
    - The start of all the phpdocs this class needs for its methods.  Ack. >_>

--- URL/Router.php
+++ URL/Router.php
@@ -29,7 +29,75 @@ class Router
 	 */
 	const EXPLODE_LIMIT = 15;
 
+	protected $base_url = '';
+
+	/**
+	 * @var array - An array of the routes we've prepared
+	 */
 	protected $routes = array();
+
+	/**
+	 * @var \OpenFlame\Framework\URL\RouteInstance - The route to return if this is the default landing page.
+	 */
+	protected $home_route;
+
+	/**
+	 * @var \OpenFlame\Framework\URL\RouteInstance - The route to return if no matching route is found.
+	 */
+	protected $error_route;
+
+	/**
+	 * Get the "base URL" of this installation, which is automatically stripped from the beginning of all requests.
+	 * @return string - The base URL we are using.
+	 */
+	public function getBaseURL()
+	{
+		return $this->base_url;
+	}
+
+	/**
+	 * Set the "base URL" for this installation, which will be stripped from the beginning of all requests.
+	 * @param string $base_url - The "base URL" which we're going to strip
+	 * @return \OpenFlame\Framework\URL\Router - Provides a fluent interface.
+	 */
+	public function setBaseURL($base_url)
+	{
+		$this->base_url = rtrim($base_url, '/'); // We don't want a trailing slash here.
+
+		return $this;
+	}
+
+	/**
+	 * Create a new route with a specific route path to cover, and a callback to assign to the route on match
+	 * @param string $route_data - The formatted route path to use for this route.
+	 * @param callable $route_callback - The callback to use when we've got the right route.
+	 * @return \OpenFlame\Framework\URL\RouteInstance - The newly created route.
+	 */
+	public function newRoute($route_data, $route_callback)
+	{
+		$route = \OpenFlame\Framework\URL\RouteInstance::newInstance()
+			->loadRawRoute($route_data)
+			->setRouteCallback($route_callback);
+
+		$this->loadRoute($route, $route->getRouteBase());
+
+		return $route;
+	}
+
+	/**
+	 * Recreate a previously constructed route using the serialized data cache of the route.
+	 * @param string $route_data - The serialized cache data to use to regenerate the route.
+	 * @return \OpenFlame\Framework\URL\RouteInstance - The newly created route.
+	 */
+	public function newCachedRoute($route_data)
+	{
+		$route = \OpenFlame\Framework\URL\RouteInstance::newInstance()
+			->loadSerializedRoute($route_data);
+
+		$this->loadRoute($route, $route->getRouteBase());
+
+		return $route;
+	}
 
 	public function newRoutes(array $routes)
 	{
@@ -51,37 +119,6 @@ class Router
 		return $this;
 	}
 
-	public function getFullRouteCache()
-	{
-		$route_cache = array();
-		foreach($this->routes as $route)
-		{
-			array_push($route_cache, $route->getSerializedRoute());
-		}
-
-		return $route_cache;
-	}
-
-	public function newRoute($route_data, $route_callback)
-	{
-		$route = \OpenFlame\Framework\URL\RouteInstance::newInstance()
-			->loadRawRoute($route_data)
-			->setRouteCallback($route_callback);
-
-		$this->loadRoute($route, $route->getRouteBase());
-
-		return $this;
-	}
-
-	public function newCachedRoute($route_data)
-	{
-		$route = \OpenFlame\Framework\URL\RouteInstance::newInstance()
-			->loadSerializedRoute($route_data);
-
-		$this->loadRoute($route, $route->getRouteBase());
-		return $this;
-	}
-
 	public function loadRoute(\OpenFlame\Framework\URL\RouteInstance $route, $route_base, $prepend = false)
 	{
 		if($prepend === true)
@@ -96,21 +133,93 @@ class Router
 		return $this;
 	}
 
+	public function getHomeRoute()
+	{
+		if($this->home_route === NULL)
+		{
+			throw new \RuntimeException('Failed to retrieve obtain "home" route; the route has not yet been defined');
+		}
+
+		return $this->home_route;
+	}
+
+	public function setHomeRoute(\OpenFlame\Framework\URL\RouteInstance $route)
+	{
+		$this->home_route = $route;
+
+		return $this;
+	}
+
+	public function getErrorRoute()
+	{
+		if($this->error_route === NULL)
+		{
+			throw new \RuntimeException('Failed to retrieve obtain "error" route; the route has not yet been defined');
+		}
+
+		return $this->error_route;
+	}
+
+	public function setErrorRoute(\OpenFlame\Framework\URL\RouteInstance $route)
+	{
+		$this->error_route = $route;
+
+		return $this;
+	}
+
+	public function getFullRouteCache()
+	{
+		$route_cache = array();
+		foreach($this->routes as $route)
+		{
+			array_push($route_cache, $route->getSerializedRoute());
+		}
+
+		$cache = array(
+			'routes'	=> $route_cache,
+			'home'		=> $this->getHomeRoute()->getSerializedRoute(),
+			'error'		=> $this->getErrorRoute()->getSerializedRoute(),
+		);
+
+		return $cache;
+	}
+
+	public function loadFromFullRouteCache(array $cache_array)
+	{
+		$this->newCachedRoutes($cache_array['routes'])
+			->setHomeRoute($this->newCachedRoute($cache_array['home']))
+			->setErrorRoute($this->newCachedRoute($cache_array['error']));
+
+		return $this;
+	}
+
 	public function processRequest($request_url)
 	{
-		// Get rid of _GET query string
+		// Get rid of the _GET stuff.
 		if (strpos($request_url, '?') !== false)
 		{
 			$request_url = substr($request_url, 0, strpos($request_url, '?'));
 		}
 
-		list($request_base, ) = explode('/', $request_url, 2);
+		// Make sure we've got beginning and ending slashes.
+		// @note can't use trim() here, it'll cause an issue on a single slash and suddenly we'd have two slashes
+		$request_url = '/' . ltrim(rtrim($request_url, '/') . '/');
+
+		// Trim out the base URL.
+		$request_url = stripos($request_url, $this->getBaseURL()) === 0 ? substr($request_url, strlen($this->getBaseURL())) : $request_url;
+
+		if($request_url = '/')
+		{
+			return $this->getHomeRoute();
+		}
+
+		list( , $request_base, ) = explode('/', $request_url, 3);
 
 		// We're cheating a bit here to boost performance under load.
 		if(!isset($this->routes[$request_base]))
 		{
 			// 404 error
-			return false;
+			return $this->getErrorRoute();
 		}
 
 		// We need to verify the request against the routes one by one, and go for the last one that works.
@@ -128,7 +237,7 @@ class Router
 		if($found !== true)
 		{
 			// 404 error
-			return false;
+			return $this->getErrorRoute();
 		}
 
 		return $route;
