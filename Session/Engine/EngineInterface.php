61748e41d4f0494301a2f29376eef2d85958ea38
Sam Thompson <sam@websyntax.net>
2011-03-10 13:45:34 -0800

    Placeholder classes created.
    Created an application/driver system that will allow the application to specify a custom session engine.

--- Session/Engine/EngineInterface.php
+++ Session/Engine/EngineInterface.php
@@ -9,19 +9,18 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework\Session;
-use OpenFlame\Framework\Core;
+namespace OpenFlame\Framework\Session\Engine;
 
 if(!defined('OpenFlame\\ROOT_PATH')) exit;
 
 /**
- * OpenFlame Web Framework - Session Handler Base
- * 	     The base class for the session handler. 
+ * OpenFlame Web Framework - Session Engine interface,
+ * 		Session engine prototype, declares required methods that a session engine must define in order to be valid.
  *
  *
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class Driver
+interface EngineInterface
 {
 }
