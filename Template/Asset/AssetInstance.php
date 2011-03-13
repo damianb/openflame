5671ea345a15a30c5173329af8f1858ce05d187c
Obsidian <stratosphere.programming@gmail.com>
2011-03-13 14:39:23 -0500

    Some more work on the asset manager.

--- Template/Asset/AssetInstance.php
+++ Template/Asset/AssetInstance.php
@@ -22,7 +22,49 @@ if(!defined('OpenFlame\\ROOT_PATH')) exit;
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class SomeClass
+class AssetInstance
 {
-	// asdf
+	protected $name = '';
+
+	protected $type = '';
+
+	protected $base_url = '';
+
+	protected $url = '';
+
+	public static function newInstance()
+	{
+		return new static();
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Get the "base URL" of this installation, which is added to the beginning of all asset URLs automatically.
+	 * @return string - The base URL we are using.
+	 */
+	public function getBaseURL()
+	{
+		return $this->base_url;
+	}
+
+	/**
+	 * Set the "base URL" for this installation, which will be added to the beginning of all asset URLs.
+	 * @param string $base_url - The "base URL" which we're going to prepend.
+	 * @return \OpenFlame\Framework\Template\Asset\AssetInstance - Provides a fluent interface.
+	 */
+	public function setBaseURL($base_url)
+	{
+		$this->base_url = rtrim($base_url, '/'); // We don't want a trailing slash here.
+
+		return $this;
+	}
 }
--- Template/Asset/Manager.php
+++ Template/Asset/Manager.php
@@ -26,7 +26,7 @@ class Manager
 {
 	protected $base_url = '';
 
-	protected $assets = '';
+	protected $assets = array();
 
 	protected $exception_on_invalid_asset = false;
 
@@ -48,7 +48,21 @@ class Manager
 	{
 		$this->base_url = rtrim($base_url, '/'); // We don't want a trailing slash here.
 
+		// We need to update the asset instances too.
+		foreach($this->assets as $type)
+		{
+			foreach($type as $asset)
+			{
+				$asset->setBaseURL($base_url);
+			}
+		}
+
 		return $this;
+	}
+
+	public function usingInvalidAssetExceptions()
+	{
+		return $this->exception_on_invalid_asset;
 	}
 
 	/**
@@ -69,5 +83,66 @@ class Manager
 	{
 		$this->exception_on_invalid_asset = false;
 		return $this;
+	}
+
+	/**
+	 *
+	 * @return \OpenFlame\Framework\Template\Asset\AssetInstance - The newly created AssetInstance object.
+	 */
+	protected function registerAsset($asset_class = '\\OpenFlame\\Framework\\Template\\Asset\\AssetInstance')
+	{
+		// We want to make sure the class exists, even with autoloading -- if we can't load it, we need to asplode.
+		if(!class_exists($asset_class, true))
+		{
+			throw new \InvalidArgumentException(sprintf('The class "%1$s" does not exist and cannot be instantiated in \\OpenFlame\\Framework\\Template\\Asset\\Manager->registerAsset()'));
+		}
+
+		return $asset_class::newInstance()->setBaseURL($this->getBaseURL());
+	}
+
+	protected function storeAsset(\OpenFlame\Framework\Template\Asset\AssetInstance $asset)
+	{
+		$this->assets[$asset->getType()][$asset->getName()] = $asset;
+
+		return $asset;
+	}
+
+	public function registerCustomAsset($type, $name)
+	{
+		return $this->storeAsset($this->registerAsset()->setType($type)->setName($name));
+	}
+
+	public function registerJSAsset($name)
+	{
+		return $this->registerCustomAsset('js', $name);
+	}
+
+	public function registerCSSAsset($name)
+	{
+		return $this->registerCustomAsset('css', $name);
+	}
+
+	public function registerImageAsset($name)
+	{
+		return $this->registerCustomAsset('image', $name);
+	}
+
+	public function getAsset($type, $name)
+	{
+		if(!isset($this->assets[$type]) || !isset($this->assets[$type][$name]))
+		{
+			if($this->usingInvalidAssetExceptions())
+			{
+				throw new \RuntimeException(sprintf('Attempted to access invalid asset "%1$s.%2$s"', $type, $name));
+			}
+			else
+			{
+				return NULL;
+			}
+		}
+		else
+		{
+			return $this->assets[$type][$name];
+		}
 	}
 }
