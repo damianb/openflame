aaa7738e2ef548a7abacf485fea3b14c2d5a5693
Sam Thompson <sam@websyntax.net>
2011-03-13 22:49:12 -0700

    Started work on the session engine base

---
+++ Session/Engine/EngineBase.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Web Framework
+ * @copyright   (c) 2010 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\Session\Engine;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Web Framework - File-based cache engine base class,
+ * 		Cache engine prototype, provides some common methods for all file-based engines to use.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+abstract class EngineBase
+{
+	/*
+	 * @var - Array of user information
+	 */
+	public $data = array();
+
+	/*
+	 * @var - Session validation information
+	 *
+	 * This property is private by design: this class should be the /only/ one
+	 * to modify or read it.
+	 */
+	private $val = array();
+
+	/*
+	 * @var - Configuration data surrounding the session
+	 */
+	private $cfg;
+
+	/*
+	 * Set cookie property method prefix
+	 */
+	const SET_COOKIE = 'setCookie';
+
+	/*
+	 * Pseudo Constructor
+	 *
+	 * Must be called at the construction of the extending class
+	 */
+	public function init()
+	{
+	}
+
+	/*
+	 * Our setters
+	 *
+	 * @param string - Method name
+	 * @param array - arguements
+	 * @return OpenFlame\Framework\Session\Engine\EngineBase - Provides a fluent interface.
+	 */
+	public function __call($name, $args)
+	{
+		// Provide for setCookie* methods
+		if(stripos($name, self::SET_COOKIE) === 0)
+		{
+			$method = strtolower(substr($name, strlen(self::SET_COOKIE)));
+			$this->cfg['cookie.' . $method] = is_numeric($args[0]) ? (int) $args[0] : (string) $args[0];
+		}
+
+		return $this;
+	}
+}
