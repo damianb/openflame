121ca3ba9ec0b1d5ca56b95d2d1a005c44e32143
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-03-20 09:54:29 -0500

    add the hollow files that the event dispatcher will fill in later

---
+++ Event/Dispatcher.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Web Framework
+ * @copyright   (c) 2010 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\Event;
+use OpenFlame\Framework\Core;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Web Framework - Event dispatcher object
+ * 	     Provides event dispatcher functionality for ease of extensibility.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+class Dispatcher
+{
+	// asdf
+}
