14a68142116446b363a10123c92c95c17eb60115
Sam Thompson <sam@websyntax.net>
2011-03-22 15:41:11 -0700

    Started working on some of the Driver code

---
+++ Session/Storage/EngineInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Web Framework
+ * @copyright   (c) 2010 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\Session\Storage;
+use \OpenFlame\Framework\Core;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Web Framework - Cache Engine interface,
+ * 		Cache engine prototype, declares required methods that a cache engine must define in order to be valid.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+interface EngineInterface
+{
+}
