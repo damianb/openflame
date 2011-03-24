a9392607e8ce9939befeaf6f2f0fd547d5842963
Sam Thompson <sam@websyntax.net>
2011-03-24 10:21:22 -0700

    Added ArrayAccess to Config.php

--- Utility/Config.php
+++ Utility/Config.php
@@ -9,19 +9,74 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework;
+namespace OpenFlame\Framework\Utility;
 
 if(!defined('OpenFlame\\ROOT_PATH')) exit;
 
 /**
- * OpenFlame Web Framework - Some class
- * 	     Some class description.
+ * OpenFlame Web Framework - General Configuration Manager
+ * 	     Provides an applicaiton-level configuration manager
  *
  *
  * @license     http://opensource.org/licenses/mit-license.php The MIT License
  * @link        https://github.com/OpenFlame/OpenFlame-Framework
  */
-class SomeClass
+class Config implements ArrayAccess
 {
-	// asdf
+	/*
+	 * @var - all the data that used by the array
+	 */
+	protected $data = array();
+
+	/*
+	 * ArrayAccess - Exists
+	 *
+	 * @param mixed - Offest to unset
+	 * @return bool - Wheather or not the offset exists
+	 */
+	public function offestExists($offset)
+	{
+		return isset($this->data[$offset]) ? true : false;
+	}
+
+	/*
+	 * ArrayAccess - Get
+	 *
+	 * @param mixed - Offest to get
+	 * @return mixed - The value at the offset
+	 */
+	public function offestGet($offset)
+	{
+		return isset($this->data[$offset]) ? $this->data[$offset] : null;
+	}
+
+	/*
+	 * ArrayAccess - Set
+	 *
+	 * @param mixed - Key to store the new value in
+	 * @param mixed - New value
+	 * @return void
+	 */
+	public function offestSet($offset, $value)
+	{
+		if($offset == null)
+		{
+			$this->data[] = $value;
+		}
+		else
+		{
+			$this->data[$offset] = $value;
+		}
+	}
+
+	/*
+	 * ArrayAccess - Unset
+	 *
+	 * @param mixed - Offest to unset
+	 * @return void
+	 */
+	public function offestUnset($offset)
+	{
+		unset($this->data[$offset];
+	}
 }
