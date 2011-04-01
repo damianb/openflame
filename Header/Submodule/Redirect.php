e1ad4ec0092ed71b2357cb465e4342063728bbcd
Obsidian <stratosphere.programming@gmail.com>
2011-04-01 17:47:01 -0500

    Redirect submodule skeleton

---
+++ Header/Submodule/Redirect.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @package     OpenFlame Web Framework
+ * @copyright   (c) 2010 OpenFlameCMS.com
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\Header;
+use OpenFlame\Framework\Core;
+
+if(!defined('OpenFlame\\ROOT_PATH')) exit;
+
+/**
+ * OpenFlame Web Framework - Header manager object
+ * 	     Takes in and manages headers that should be sent upon page display.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+class Redirect implements \OpenFlame\Framework\Header\Submodule\SubmoduleInterface
+{
+	// asdf
+}
