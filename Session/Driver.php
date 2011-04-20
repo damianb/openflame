22a620b0d00affba741f031357e3981e81c87cfb
Sam Thompson <sam@websyntax.net>
2011-04-20 16:22:02 -0700

    The driver is slowly taking shape

--- Session/Driver.php
+++ Session/Driver.php
@@ -27,22 +27,99 @@ class Driver
 	/*
 	 * @var \OpenFlame\Framework\Session\Storage\EngineInterface
 	 */
-	protected $engine;
+	protected $storageEngine;
+
+	/*
+	 * @var \OpenFlame\Framework\Session\Client\EngineInterface
+	 */
+	protected $clientEngine;
 
 	/*
 	 * @var session data
 	 */
 	public $data = array();
 
+	/*
+	 *  @var session id
+	 */
+	public $sid = '';
+
+	/*
+	 *  @var user ID
+	 */
+	public $uid = '';
+
+	/*
+	 *  @var autlogin key
+	 */
+	public $autologinKey = '';
+
 	/**
 	 * Sets the session storage engine to be used
 	 * @param \OpenFlame\Framework\Session\Storage\EngineInterface - The Session engine to use.
 	 * @return \OpenFlame\Framework\Session\Driver - Provides a fluent interface.
 	 */
-	public function setEngine(\OpenFlame\Framework\Session\Storage\EngineInterface $engine)
+	public function setStorageEngine(\OpenFlame\Framework\Session\Storage\EngineInterface $engine)
 	{
-		$this->engine = $engine;
+		$this->storageEngine = $engine;
 
 		return $this;
+	}
+
+	/**
+	 * Sets the Session Client-side identification engine
+	 * @param \OpenFlame\Framework\Session\Client\EngineInterface - The Session engine to use.
+	 * @return \OpenFlame\Framework\Session\Driver - Provides a fluent interface.
+	 */
+	public function setStorageEngine(\OpenFlame\Framework\Session\Client\EngineInterface $engine)
+	{
+		$this->clientEngine = $engine;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the session storage engine to be used
+	 * @param array - Options to feed the engine
+	 * @return \OpenFlame\Framework\Session\Driver - Provides a fluent interface.
+	 */
+	public function setEngineOptions($options)
+	{
+		$this->storageEngine->init($options);
+		$this->clientEngine->setOptions($options);
+
+		return $this;
+	}
+
+	/**
+	 * Start the session 
+	 * @return void
+	 */
+	public function start()
+	{
+		$this->clientEngine->onStart();
+	}
+
+	/**
+	 * Login - Mainly gets handled by the application
+	 *
+	 * @param string - Username
+	 * @param string - Password (still in plain text)
+	 * @param boolean - Was the autologin box checked?
+	 * @param mixed - Flags that will be passed to the application
+	 * @return bool - true if logged in, false if not
+	 */
+	public function login($username, $password, $autologin = false, $flags = array())
+	{
+		$this->clientEngine->onLogin();
+	}
+
+	/**
+	 * Start the session 
+	 * @return void
+	 */
+	public function kill()
+	{
+		$this->clientEngine->onKill();
 	}
 }
