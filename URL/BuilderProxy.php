0dd63ddd627cb3895e960b014379b8b26b92c573
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-04-26 12:54:39 -0500

    Docs for the builder proxy object

--- URL/BuilderProxy.php
+++ URL/BuilderProxy.php
@@ -29,17 +29,28 @@ class BuilderProxy
 	 */
 	protected $builder;
 
+	/**
+	 * Constructor
+	 * @param \OpenFlame\Framework\URL\Builder $manager - The URL builder object.
+	 * @return void
+	 */
 	public function __construct(\OpenFlame\Framework\URL\Builder $builder)
 	{
 		$this->builder = $builder;
 	}
 
+	/**
+	 * Magic proxy method for generating URLs, uses URL patterns with printf formats to resolve the URL to use.
+	 * @param string $name - The method name called (used to figure out the pattern to use for URL generation).
+	 * @param string $arguments - The arguments to pass to the vsprintf() call as the variable components of the URL pattern.
+	 * @return string - Returns an empty string if no URL pattern specified, or returns a string containing the generated URL.
+	 */
 	public function __call($name, array $arguments)
 	{
 		$pattern = $this->builder->getPattern($name);
 		if($pattern === NULL)
 		{
-			return NULL;
+			return '';
 		}
 
 		return $this->builder->getBaseURL() . '/' . vsprintf($pattern, $arguments);
