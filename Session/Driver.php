caeca44738dff913b0fdfa85a45435cc40402574
Obsidian <stratosphere.programming@gmail.com>
2011-05-12 14:54:51 -0500

    derp

--- Session/Driver.php
+++ Session/Driver.php
@@ -230,7 +230,7 @@ class Driver
 				{
 					$returns = array();
 				}
-				$this->data = array_merge($returns, $this->data);
+				$this->data = array_merge($returns['data'], $this->data);
 
 				$paramsToSend['uid'] = $this->uid;
 				$paramsToSend['alk'] = $this->alk;
