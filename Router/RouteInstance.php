ae2c1cd799cc44b2a3a8227a01655a0059a5c121
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-08-13 07:19:01 -0500

    deprec and replace method names here with some shorter ones, for instance properties

--- Router/RouteInstance.php
+++ Router/RouteInstance.php
@@ -286,7 +286,7 @@ class RouteInstance
 				}
 				if(isset($map[$i]))
 				{
-					$this->setRequestDataPoint($map[$i]['entry'], $matches[$j]);
+					$this->set($map[$i]['entry'], $matches[$j]);
 					$j++;
 				}
 
@@ -300,8 +300,47 @@ class RouteInstance
 	 * Get a request URL variable extracted from the request.
 	 * @param string $point - The name of the variable to retrieve.
 	 * @return mixed - returns NULL if no such variable, or the variable's data.
+	 *
+	 * @deprecated since 1.2.0
 	 */
 	public function getRequestDataPoint($point)
+	{
+		trigger_error('\\OpenFlame\\Framework\\Router\\RouteInstance->getRequestDataPoint() is deprecated', E_USER_DEPRECATED);
+
+		if(isset($this->data[(string) $point]))
+		{
+			return $this->data[(string) $point];
+		}
+		else
+		{
+			return NULL;
+		}
+
+	}
+
+	/**
+	 * Set a request URL variable extracted from the current request.
+	 * @param string $point - The name of the variable to store this as.
+	 * @param mixed $data - The data to store.
+	 * @return \OpenFlame\Framework\Router\RouteInstance - Provides a fluent interface.
+	 *
+	 * @deprecated since 1.2.0
+	 */
+	public function setRequestDataPoint($point, $data)
+	{
+		trigger_error('\\OpenFlame\\Framework\\Router\\RouteInstance->setRequestDataPoint() is deprecated', E_USER_DEPRECATED);
+
+		$this->data[(string) $point] = $data;
+
+		return $this;
+	}
+
+	/**
+	 * Get a request URL variable extracted from the request.
+	 * @param string $point - The name of the variable to retrieve.
+	 * @return mixed - returns NULL if no such variable, or the variable's data.
+	 */
+	public function get($point)
 	{
 		if(isset($this->data[(string) $point]))
 		{
@@ -320,7 +359,7 @@ class RouteInstance
 	 * @param mixed $data - The data to store.
 	 * @return \OpenFlame\Framework\Router\RouteInstance - Provides a fluent interface.
 	 */
-	public function setRequestDataPoint($point, $data)
+	public function set($point, $data)
 	{
 		$this->data[(string) $point] = $data;
 
