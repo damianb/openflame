9723e9d1511b2b26f1745671cb2afc6fe0a95a4d
damianb <damian@emberlabs.org>
2011-09-17 12:31:27 -0500

    update version stamp for potential future releases

--- Core.php
+++ Core.php
@@ -34,7 +34,7 @@ class Core
 	/**
 	 * @var string - The version for the Framework
 	 */
-	private static $version = '1.2.0';
+	private static $version = '1.2.0-dev';
 
 	/**
 	 * @var string - The commit ID for phar-packaged forms of the framework (considering "unstable" development builds)
