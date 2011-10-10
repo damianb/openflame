e501ef3c73fc3584a42f3bee390295f19495260a
damianb <damian@emberlabs.org>
2011-10-10 13:38:03 -0500

    really fix the json regexp.

--- Utility/JSON.php
+++ Utility/JSON.php
@@ -53,7 +53,7 @@ abstract class JSON
 			return NULL;
 		}
 
-		$data = json_decode(preg_replace("/\s#.*?\n/", '', $json), true);
+		$data = json_decode(preg_replace("/([$|\n])\s*#(.*)?\n/", '$1', $json), true);
 
 		if($data === NULL)
 		{
