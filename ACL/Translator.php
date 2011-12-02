ba953e74dd8c49ebbdb9e68c890110a0259e6e18
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-12-02 11:37:12 -0600

    Starting work on the ACL

---
+++ ACL/Translator.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @package     openflame-framework
+ * @subpackage  ACL
+ * @copyright   (c) 2010 - 2011 openflame-project.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\ACL;
+use \OpenFlame\Framework\Core;
+
+/**
+ * OpenFlame Framework - ACL binary auth compiler object
+ * 	     Takes provided auth flags, group auth settings & inheritances and translates them into a raw auth string for use with the ACL\Auth class.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+class Translator
+{
+	// asdf
+}
