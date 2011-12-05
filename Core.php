812af0cd472343304a9a49fcf0db2c5262b6e913
Damian Bushong <damian@emberlabs.org>
2011-12-05 16:14:07 -0600

    release version 1.3.1

--- Core.php
+++ Core.php
@@ -34,7 +34,7 @@ class Core
 	/**
 	 * @var string - The version for the Framework
 	 */
-	private static $version = '1.3.1-dev';
+	private static $version = '1.3.1';
 
 	/**
 	 * @var string - The commit ID for phar-packaged forms of the framework (considering "unstable" development builds)
