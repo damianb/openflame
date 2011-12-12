f73b7211ed66611b54cd330e2fc0a32f511ccb10
damianb <damian@emberlabs.org>
2011-12-12 00:21:42 -0600

    Tweaks to internal exception classes

--- Event/Internal/EventException.php
+++ Event/Internal/EventException.php
@@ -10,7 +10,8 @@
  * Minimum Requirement: PHP 5.3.0
  */
 
-namespace OpenFlame\Framework\Core\Internal;
+namespace OpenFlame\Framework\Event\Internal;
+use OpenFlame\Framework\Core\Internal\OpenFlameException;
 
 /**
  * OpenFlame Framework - Event dispatcher/instance/scheduler exception class
