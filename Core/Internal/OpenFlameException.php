f6375a846d237a7f1668f69236ea351b7f1a7855
Damian Bushong <141002651+damianb@users.noreply.github.com>
2011-12-12 00:12:26 -0600

    Internal exception classes

---
+++ Core/Internal/OpenFlameException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @package     openflame-framework
+ * @subpackage  core
+ * @copyright   (c) 2010 - 2011 openflame-project.org
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ *
+ * Minimum Requirement: PHP 5.3.0
+ */
+
+namespace OpenFlame\Framework\Core\Internal;
+
+/**
+ * OpenFlame Framework - Base exception class
+ * 	     Base exception class for all OpenFlame Framework exceptions.
+ *
+ *
+ * @license     http://opensource.org/licenses/mit-license.php The MIT License
+ * @link        https://github.com/OpenFlame/OpenFlame-Framework
+ */
+class OpenFlameException extends \Exception {}
