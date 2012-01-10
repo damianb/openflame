668a40f524a0d36489492dbf2075d22dc74512d8
Damian Bushong <damian@emberlabs.org>
2012-01-09 20:22:01 -0600

    update version stamp for rc2

--- Core/Core.php
+++ Core/Core.php
@@ -34,7 +34,7 @@ class Core
 	/**
 	 * @var string - The version for the Framework
 	 */
-	private static $version = '2.0.0-dev';
+	private static $version = '2.0.0-rc2';
 
 	/**
 	 * @var array - Array of settings we have loaded and stored
